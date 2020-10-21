ba140f336b4615f5b80510a8e6a4d08d6a5e464a
Nick <CoffeeBeforeArch@gmail.com>
2020-10-20 23:41:25 -0500

    update backoff

--- backoff/backoff.cpp
+++ backoff/backoff.cpp
@@ -9,9 +9,11 @@
 #include <atomic>
 #include <cstdint>
 #include <thread>
+#include <vector>
 
 // Simple Spinlock
 // Lock now performs local spinning
+// Lock now does backoff
 struct Spinlock {
   // Lock is just an atomic bool
   std::atomic<bool> locked{false};
@@ -21,24 +23,24 @@ struct Spinlock {
     // Keep trying
     while (1) {
       // Try and grab the lock
-      // Return if we get the lock
+      // Exit if we got the lock
       if (!locked.exchange(true)) return;
 
       // If we didn't get the lock, just read the value which gets cached
       // locally. This leads to less traffic.
-      // Designed to improve the performance of spin-wait loops.
+      // Each iteration we can also call pause to limit the number of writes.
       while (locked.load()) __builtin_ia32_pause();
     }
   }
 
   // Unlocking mechanism
-  // Just set the lock to free (0)
+  // Just set the lock to free (false)
   // Can also use the assignment operator
   void unlock() { locked.store(false); }
 };
 
 // Increment val once each time the lock is acquired
-void inc_small(Spinlock &s, std::int64_t &val) {
+void inc(Spinlock &s, std::int64_t &val) {
   for (int i = 0; i < 100000; i++) {
     s.lock();
     val++;
@@ -46,26 +48,8 @@ void inc_small(Spinlock &s, std::int64_t &val) {
   }
 }
 
-// Increment val 100 times each time the lock is acquired
-void inc_medium(Spinlock &s, std::int64_t &val) {
-  for (int i = 0; i < 1000; i++) {
-    s.lock();
-    for (int j = 0; j < 100; j++) benchmark::DoNotOptimize(val++);
-    s.unlock();
-  }
-}
-
-// Increment val 1000 times each time the lock is acquired
-void inc_large(Spinlock &s, std::int64_t &val) {
-  for (int i = 0; i < 100; i++) {
-    s.lock();
-    for (int j = 0; j < 1000; j++) benchmark::DoNotOptimize(val++);
-    s.unlock();
-  }
-}
-
 // Small Benchmark
-static void backoff_small(benchmark::State &s) {
+static void backoff(benchmark::State &s) {
   // Sweep over a range of threads
   auto num_threads = s.range(0);
 
@@ -81,74 +65,17 @@ static void backoff_small(benchmark::State &s) {
   // Timing loop
   for (auto _ : s) {
     for (auto i = 0u; i < num_threads; i++) {
-      threads.emplace_back([&] { inc_small(sl, val); });
+      threads.emplace_back([&] { inc(sl, val); });
     }
     // Join threads
     for (auto &thread : threads) thread.join();
     threads.clear();
   }
 }
-BENCHMARK(backoff_small)
-    ->DenseRange(1, std::thread::hardware_concurrency())
+BENCHMARK(backoff)
+    ->RangeMultiplier(2)
+    ->Range(1, std::thread::hardware_concurrency())
     ->UseRealTime()
     ->Unit(benchmark::kMillisecond);
-
-// Medium Benchmark
-static void backoff_medium(benchmark::State &s) {
-  // Sweep over a range of threads
-  auto num_threads = s.range(0);
-
-  // Value we will increment
-  std::int64_t val = 0;
-
-  // Allocate a vector of threads
-  std::vector<std::thread> threads;
-  threads.reserve(num_threads);
-
-  Spinlock sl;
-
-  // Timing loop
-  for (auto _ : s) {
-    for (auto i = 0u; i < num_threads; i++) {
-      threads.emplace_back([&] { inc_medium(sl, val); });
-    }
-    // Join threads
-    for (auto &thread : threads) thread.join();
-    threads.clear();
-  }
-}
-BENCHMARK(backoff_medium)
-    ->DenseRange(1, std::thread::hardware_concurrency())
-    ->UseRealTime()
-    ->Unit(benchmark::kMicrosecond);
-
-// Large Benchmark
-static void backoff_large(benchmark::State &s) {
-  // Sweep over a range of threads
-  auto num_threads = s.range(0);
-
-  // Value we will increment
-  std::int64_t val = 0;
-
-  // Allocate a vector of threads
-  std::vector<std::thread> threads;
-  threads.reserve(num_threads);
-
-  Spinlock sl;
-
-  // Timing loop
-  for (auto _ : s) {
-    for (auto i = 0u; i < num_threads; i++) {
-      threads.emplace_back([&] { inc_large(sl, val); });
-    }
-    // Join threads
-    for (auto &thread : threads) thread.join();
-    threads.clear();
-  }
-}
-BENCHMARK(backoff_large)
-    ->DenseRange(1, std::thread::hardware_concurrency())
-    ->UseRealTime()
-    ->Unit(benchmark::kMicrosecond);
 
 BENCHMARK_MAIN();
