be8fee727934d270063418fadaf972302f4f7442
Nick <CoffeeBeforeArch@gmail.com>
2020-10-24 01:02:46 -0500

    update exponential backoff

--- exp_backoff/exp_backoff.cpp
+++ exp_backoff/exp_backoff.cpp
@@ -10,16 +10,18 @@
 #include <atomic>
 #include <cstdint>
 #include <thread>
+#include <vector>
 
 // Simple Spinlock
 // Lock now performs local spinning
+// Lock now performs exponential backoff
 struct Spinlock {
   // Lock is just an atomic bool
   std::atomic<bool> locked{false};
 
   // Constants for number of backoff iterations
   const int MAX_BACKOFF = 1 << 10;
-  const int MIN_BACKOFF = 1;
+  const int MIN_BACKOFF = 4;
 
   // Locking mechanism
   void lock() {
@@ -43,13 +45,13 @@ struct Spinlock {
   }
 
   // Unlocking mechanism
-  // Just set the lock to free (0)
+  // Just set the lock to free (false)
   // Can also use the assignment operator
   void unlock() { locked.store(false, std::memory_order_release); }
 };
 
 // Increment val once each time the lock is acquired
-void inc_small(Spinlock &s, std::int64_t &val) {
+void inc(Spinlock &s, std::int64_t &val) {
   for (int i = 0; i < 100000; i++) {
     s.lock();
     val++;
@@ -57,26 +59,8 @@ void inc_small(Spinlock &s, std::int64_t &val) {
   }
 }
 
-// Increment val 100 times each time the lock is acquired
-void inc_medium(Spinlock &s, std::int64_t &val) {
-  for (int i = 0; i < 1000; i++) {
-    s.lock();
-    for (int j = 0; j < 100; j++) benchmark::DoNotOptimize(val++);
-    s.unlock();
-  }
-}
-
-// Increment val 1000 times each time the lock is acquired
-void inc_large(Spinlock &s, std::int64_t &val) {
-  for (int i = 0; i < 100; i++) {
-    s.lock();
-    for (int j = 0; j < 1000; j++) benchmark::DoNotOptimize(val++);
-    s.unlock();
-  }
-}
-
 // Small Benchmark
-static void exp_backoff_small(benchmark::State &s) {
+static void exp_backoff(benchmark::State &s) {
   // Sweep over a range of threads
   auto num_threads = s.range(0);
 
@@ -92,74 +76,17 @@ static void exp_backoff_small(benchmark::State &s) {
   // Timing loop
   for (auto _ : s) {
     for (auto i = 0u; i < num_threads; i++) {
-      threads.emplace_back([&] { inc_small(sl, val); });
+      threads.emplace_back([&] { inc(sl, val); });
     }
     // Join threads
     for (auto &thread : threads) thread.join();
     threads.clear();
   }
 }
-BENCHMARK(exp_backoff_small)
-    ->DenseRange(1, std::thread::hardware_concurrency())
+BENCHMARK(exp_backoff)
+    ->RangeMultiplier(2)
+    ->Range(1, std::thread::hardware_concurrency())
     ->UseRealTime()
     ->Unit(benchmark::kMillisecond);
-
-// Medium Benchmark
-static void exp_backoff_medium(benchmark::State &s) {
-  // Sweep over a range of threads
-  auto num_threads = s.range(0);
-
-  // Value we will increment
-  std::int64_t val = 0;
-
-  // Allocate a vector of threads
-  std::vector<std::thread> threads;
-  threads.reserve(num_threads);
-
-  Spinlock sl;
-
-  // Timing loop
-  for (auto _ : s) {
-    for (auto i = 0u; i < num_threads; i++) {
-      threads.emplace_back([&] { inc_medium(sl, val); });
-    }
-    // Join threads
-    for (auto &thread : threads) thread.join();
-    threads.clear();
-  }
-}
-BENCHMARK(exp_backoff_medium)
-    ->DenseRange(1, std::thread::hardware_concurrency())
-    ->UseRealTime()
-    ->Unit(benchmark::kMicrosecond);
-
-// Large Benchmark
-static void exp_backoff_large(benchmark::State &s) {
-  // Sweep over a range of threads
-  auto num_threads = s.range(0);
-
-  // Value we will increment
-  std::int64_t val = 0;
-
-  // Allocate a vector of threads
-  std::vector<std::thread> threads;
-  threads.reserve(num_threads);
-
-  Spinlock sl;
-
-  // Timing loop
-  for (auto _ : s) {
-    for (auto i = 0u; i < num_threads; i++) {
-      threads.emplace_back([&] { inc_large(sl, val); });
-    }
-    // Join threads
-    for (auto &thread : threads) thread.join();
-    threads.clear();
-  }
-}
-BENCHMARK(exp_backoff_large)
-    ->DenseRange(1, std::thread::hardware_concurrency())
-    ->UseRealTime()
-    ->Unit(benchmark::kMicrosecond);
 
 BENCHMARK_MAIN();
