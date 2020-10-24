cd296aaba8e939bd7ba1edeb011de31117ff4636
Nick <CoffeeBeforeArch@gmail.com>
2020-10-23 22:38:50 -0500

    add tests for power

---
+++ backoff/backoff_set_iters.cpp
@@ -0,0 +1,77 @@
+// This program benchmarks an improved spinlock C++
+// Optimizations:
+//  1.) Spin locally
+//  2.) Backoff
+// By: Nick from CoffeeBeforeArch
+
+#include <benchmark/benchmark.h>
+
+#include <atomic>
+#include <cstdint>
+#include <thread>
+#include <vector>
+
+// Simple Spinlock
+// Lock now performs local spinning
+// Lock now does backoff
+struct Spinlock {
+  // Lock is just an atomic bool
+  std::atomic<bool> locked{false};
+
+  // Locking mechanism
+  void lock() {
+    // Keep trying
+    while (1) {
+      // Try and grab the lock
+      // Exit if we got the lock
+      if (!locked.exchange(true)) return;
+
+      // If we didn't get the lock, just read the value which gets cached
+      // locally. This leads to less traffic.
+      // Each iteration we can also call pause to limit the number of writes.
+      while (locked.load()) __builtin_ia32_pause();
+    }
+  }
+
+  // Unlocking mechanism
+  // Just set the lock to free (false)
+  // Can also use the assignment operator
+  void unlock() { locked.store(false); }
+};
+
+// Increment val once each time the lock is acquired
+void inc(Spinlock &s, std::int64_t &val) {
+  for (int i = 0; i < 100000; i++) {
+    s.lock();
+    val++;
+    s.unlock();
+  }
+}
+
+// Small Benchmark
+static void backoff(benchmark::State &s) {
+  // Sweep over a range of threads
+  auto num_threads = s.range(0);
+
+  // Value we will increment
+  std::int64_t val = 0;
+
+  // Allocate a vector of threads
+  std::vector<std::thread> threads;
+  threads.reserve(num_threads);
+
+  Spinlock sl;
+
+  // Timing loop
+  for (auto _ : s) {
+    for (auto i = 0u; i < num_threads; i++) {
+      threads.emplace_back([&] { inc(sl, val); });
+    }
+    // Join threads
+    for (auto &thread : threads) thread.join();
+    threads.clear();
+  }
+}
+BENCHMARK(backoff)->Arg(4)->Unit(benchmark::kMillisecond)->Iterations(100);
+
+BENCHMARK_MAIN();
